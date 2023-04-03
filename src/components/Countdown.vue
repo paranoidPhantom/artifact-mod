35e9c148cbf9e1959bbb09abce5cf2294f060ccb
Andrei <andrei.hudalla@outlook.com>
2023-04-03 20:10:59 +0300

    Update Countdown.vue

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -29,9 +29,8 @@ const timerCheckInterval = setInterval(() => {
 </script>
 
 <template>
-    <img src="/images/sidormeme.webp" style="width: 100%;">
-    <h1>У нас технические шоколадки, а что поделаешь...</h1>
-    <h2><i>...хрен его знает на кой ляд тебе этот Артефакт сдался...</i></h2>
+    <img src="/images/sidormeme.webp" style="width: 100%; filter: sepia(0.7) saturate(2); border-radius: 3rem;">
+    <h1>🍫 У нас технические шоколадки, а что поделаешь...</h1>
     <hr v-if="!timerValue">
     <h2 v-if="timerValue" id="countdown">{{ timerValue }}</h2>
     <h2 class="updsoon" v-else>Сайт скоро обновиться!</h2>
